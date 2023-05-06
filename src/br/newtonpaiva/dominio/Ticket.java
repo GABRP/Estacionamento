4bc77d7461bfd4b4d8b2a2c3c0b1b66dee2fca4e
12115884 <f>
2023-05-05 21:24:14 -0300

    feat: implementado as class ticket e veiculo

---
+++ src/br/newtonpaiva/dominio/Ticket.java
@@ -0,0 +1,50 @@
+package br.newtonpaiva.dominio;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Ticket {
+    private LocalDateTime inicio;
+    private LocalDateTime fim;
+    private Veiculo veiculo;
+
+    public Ticket(LocalDateTime inicio, Veiculo veiculo) {
+        this.inicio = inicio;
+        this.veiculo = veiculo;
+    }
+
+    public Object fechar() {
+        this.fim = LocalDateTime.now();
+
+        long horas = ChronoUnit.HOURS.between(inicio, fim);
+        long dias = ChronoUnit.DAYS.between(inicio, fim);
+        long meses = ChronoUnit.MONTHS.between(inicio, fim);
+
+        //logica
+        if (horas < 12) {
+            return new BigDecimal(horas * 10);
+        } else if (dias < 15) {
+            if (dias == 0)
+                dias = 1;
+            return new BigDecimal(dias * 40);
+        } else {
+            if (meses == 0) {
+                meses = 1;
+                return new BigDecimal(meses * 200);
+            }
+        }
+
+        public LocalDateTime getInicio () {
+            return inicio;
+        }
+
+        public LocalDateTime getFim () {
+            return fim;
+        }
+
+        public Veiculo getVeiculo () {
+            return veiculo;
+        }
+    }
+}
